5a30ef738ca2fda83918b82dbe8eda5f24569740
Viktor <victorharasymiv97@gmail.com>
2025-04-19 17:45:02 +0200

    new update (subscribe modal) v1

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -42,74 +42,87 @@ function Subscribe({ isSubscribe, openSubscribe }) {
           },
         }}
       >
-        <DialogTitle>Subscribe for updates</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Subscribe for exclusive early sale access and new arrivals.
-          </DialogContentText>
-          <div className={css.radio_tile}>
-            {["Women", "Men", "Girls", "Boys"].map((gender) => (
-              <div key={gender}>
-                <input type="radio" id={gender} name="gender" value={gender} />
-                <label className="radio_label" htmlFor={gender}>
-                  {gender}
-                </label>
-              </div>
-            ))}
-          </div>
-          <label className={css.email_label} htmlFor="email">
-            <span>Email</span>
-            <input
-              type="email"
-              id="name"
-              name="email"
-              placeholder="Your working email"
-              pattern="^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$"
-              maxLength={30}
-              autoFocus
-              required
-            />
-            <Button type="submit" className={css.subscribe_button}>
-              Subscribe
-            </Button>
-          </label>
-          <label htmlFor="checkbox" className={css.subscribe_checkbox}>
-            <span className={css.checkbox_label}>
-              I agree to receive communications from Createx Store.
-            </span>
-            <div className={css.custom_checkbox}>
-              <input
-                className={css.checkbox_input}
-                type="checkbox"
-                id="checkbox"
-                name="checkbox"
-                defaultChecked={true}
-              />
-              <div className={css.primary_checkbox}></div>
+        <div className={css.success_tile}>
+          <h2 className={css.subscribe_title}>Subscribe for updates</h2>
+          <DialogContent>
+            <h3 className={css.subscribe_sub_title}>
+              Subscribe for exclusive early sale access and new arrivals.
+            </h3>
+            <div className={css.radio_tile}>
+              {["Women", "Men", "Girls", "Boys"].map((gender) => (
+                <div key={gender}>
+                  <input
+                    type="radio"
+                    id={gender}
+                    name="gender"
+                    value={gender}
+                  />
+                  <label className="radio_label" htmlFor={gender}>
+                    {gender}
+                  </label>
+                </div>
+              ))}
             </div>
-          </label>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={openSubscribe}>Cancel</Button>
-        </DialogActions>
+            <label className={css.email_label} htmlFor="email">
+              <span>Email</span>
+              <input
+                type="email"
+                id="name"
+                name="email"
+                placeholder="Your working email"
+                pattern="^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$"
+                maxLength={30}
+                autoFocus
+                required
+              />
+              <Button type="submit" className={css.subscribe_button}>
+                Subscribe
+              </Button>
+            </label>
+            <label htmlFor="checkbox" className={css.subscribe_checkbox}>
+              <span className={css.checkbox_label}>
+                I agree to receive communications from Createx Store.
+              </span>
+              <div className={css.custom_checkbox}>
+                <input
+                  className={css.checkbox_input}
+                  type="checkbox"
+                  id="checkbox"
+                  name="checkbox"
+                  defaultChecked={true}
+                />
+                <div className={css.primary_checkbox}></div>
+              </div>
+            </label>
+          </DialogContent>
+          <DialogActions>
+            <Button className={css.cancel_button} onClick={openSubscribe}>
+              Cancel
+            </Button>
+          </DialogActions>
+        </div>
       </Dialog>
       <Dialog
         open={successModal}
         onClose={openSubscribe}
-        className="success_dialog_tile"
+        className={css.success_dialog_tile}
       >
-        <h2 className={css.success_title}> Success subscribe</h2>
-        <span className={css.success_info}>
-          <b>Email:</b> {formData.email}
-        </span>
-        <span className={css.success_info}>
-          <b>Gender:</b> {formData.gender}
-        </span>
-        <span className={css.success_info}>
-          <b>Config:</b> {formData.checkbox}
-        </span>
+        <div className={css.success_info_tile}>
+          <h2 className={css.success_title}> Success subscribe</h2>
+          <span className={css.success_info}>
+            <b>Email:</b> {formData.email}
+          </span>
+          <span className={css.success_info}>
+            <b>Gender:</b> {formData.gender}
+          </span>
+          <span className={css.success_info}>
+            <b>Config:</b> {formData.checkbox}
+          </span>
+        </div>
         <DialogActions>
-          <Button onClick={openSubscribe}>Cancel</Button>
+          <Button onClick={openSubscribe} className={css.cancel_button}>
+            Cancel
+          </Button>
         </DialogActions>
       </Dialog>
     </>
